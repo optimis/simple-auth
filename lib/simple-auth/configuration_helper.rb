661d845d4b998657ef045161742a846ab8af7c00
Umang Chouhan + Ehren Murdick <uchouhan+uc+em@optimiscorp.com>
2010-07-16 15:44:24 -0700

    Adding SimpleAuth#configure as a module_function

--- lib/simple-auth/configuration_helper.rb
+++ lib/simple-auth/configuration_helper.rb
@@ -1,6 +1,6 @@
 module SimpleAuth
   class HostConfiguration
-    def method_missing(name, value)
+    def self.method_missing(name, value)
       if name.to_s == RAILS_ENV
         ActionController::Base.send(:define_method, 'api_host', lambda { 'http://' + value })
       end
@@ -8,25 +8,24 @@ module SimpleAuth
   end
 
   class PathConfiguration
-    def method_missing(name, value)
+    def self.method_missing(name, value)
       ActionController::Base.send(:define_method, "#{name}_path", lambda { value })
       ActionController::Base.send(:define_method, "#{name}_url", lambda { api_host + '/' + value })
     end
   end
 
-  def self.configure(&block)
-    @host_configuration = HostConfiguration.new
-    @path_configuration = PathConfiguration.new
+  def configure(&block)
     instance_eval(&block)
   end
+  module_function :configure
 
   def host(&block)
-    @host_configuration.instance_eval(&block)
+    HostConfiguration.instance_eval(&block)
   end
   module_function :host
 
   def paths(&block)
-    @path_configuration.instance_eval(&block)
+    PathConfiguration.instance_eval(&block)
   end
   module_function :paths
 end
