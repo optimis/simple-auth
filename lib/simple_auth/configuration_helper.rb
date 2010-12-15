c1709f15a8aaabeac5f13af0b3c84ec8b220538d
Josh Moore <joshsmoore@gmail.com>
2010-12-16 00:43:24 +0800

    * Added a helper to allow configuration from YAML * Allows the host to have a protocol (if no protocol is given will default to http://

--- lib/simple_auth/configuration_helper.rb
+++ lib/simple_auth/configuration_helper.rb
@@ -6,7 +6,7 @@ module SimpleAuth
 
     def self.method_missing(name, value)
       if name.to_s == RAILS_ENV
-        absolute_path = 'http://' + value
+        absolute_path = value =~ /https?:\/\// ? value : 'http://' + value
         path_getter = lambda { absolute_path }
 
         SimpleAuth::Api.api_host = absolute_path
@@ -39,4 +39,19 @@ module SimpleAuth
     PathConfiguration.instance_eval(&block)
   end
   module_function :paths
+
+  def configure_from_yml(yml_hash)
+    yml_hash['host'].keys.each do |env_name|
+      host do
+        send(env_name, yml_hash['host'][env_name])
+      end
+
+      yml_hash['paths'].keys.each do |path_name|
+        paths do
+          send(path_name, yml_hash['paths'][path_name])
+        end
+      end
+    end
+  end
+  module_function :configure_from_yml
 end
--- spec/lib/simple_auth/configuration_helper_spec.rb
+++ spec/lib/simple_auth/configuration_helper_spec.rb
@@ -1,11 +1,40 @@
 require 'spec/spec_helper'
 
-class ApplicationController < ActionController::Base; end
+class ApplicationController < ActionController::Base;
+end
 
 describe ApplicationController do
   context '#api' do
+    after :each do
+      SimpleAuth.configure do
+        host do
+          test 'test.domain.local'
+        end
+      end
+    end
     it 'should initialize the api path from the settings' do
       subject.api_host.should == 'http://test.domain.local'
+    end
+
+
+    it 'should not add the protocol if the http protocol is already added' do
+      SimpleAuth.configure do
+        host do
+          test 'http://test.domain.local'
+        end
+      end
+
+      subject.api_host.should == 'http://test.domain.local'
+    end
+
+    it 'should not add the protocol if the https protocol is already added' do
+      SimpleAuth.configure do
+        host do
+          test 'https://test.domain.local'
+        end
+      end
+
+      subject.api_host.should == 'https://test.domain.local'
     end
   end
 
@@ -18,6 +47,32 @@ describe ApplicationController do
   context '#url' do
     it 'should initialize the login path from the settings' do
       subject.login_url.should == 'http://test.domain.local/login'
+    end
+  end
+
+  context '#configure_from_yml' do
+    before :each do
+      @yaml = "host :\n  test : http://yaml.domain.local\npaths :\n  my_path : mypath"
+    end
+
+    after :each do
+      SimpleAuth.configure do
+        host do
+          test 'https://test.domain.local'
+        end
+      end
+    end
+
+    it 'should load the host configuration from YAML' do
+      SimpleAuth.configure_from_yml YAML.load(@yaml)
+
+      subject.api_host.should == 'http://yaml.domain.local'
+    end
+
+    it 'should load paths from YAML' do
+      SimpleAuth.configure_from_yml YAML.load(@yaml)
+
+      subject.my_path_path.should == 'mypath'
     end
   end
 end
